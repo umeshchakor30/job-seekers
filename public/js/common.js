ead43c9fcb63df968652dd2c32c0f520f171378a
umeshchakor30 <umeshchakor30@gmail.com>
2022-03-24 21:05:05 +0530

    login page updates

--- public/js/common.js
+++ public/js/common.js
@@ -1,26 +1,46 @@
 // For login
 const login = () => {
-  var login_email = document.getElementById("login_email").value;
+  var login_email = document.getElementById("login_user").value;
   var login_password = document.getElementById("login_password").value;
 
-  fetch("/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      login_email: login_email,
-      login_password: login_password,
-    }),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      if (data.message == "ERROR") {
-        //document.getElementById("message").innerHTML=data.msgtype;
-      }
+  document.getElementById("err_login_user").innerHTML = "";
+  document.getElementById("err_login_password").innerHTML = "";
+  var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
-      if (data.message == "SUCCESS") {
-        location.href = "/thankyou";
-      }
-    });
+  if (login_email.length == 0) {
+    document.getElementById("err_login_user").innerHTML =
+      "Please enter a email id";
+    document.getElementById("login_user").focus();
+    return false;
+  } else if (reg.test(login_email) == false) {
+    document.getElementById("err_login_user").innerHTML =
+      "Please enter a valid email id";
+    return false;
+  } else if (login_password.length == 0) {
+    document.getElementById("err_login_password").innerHTML =
+      "Please enter a password";
+    document.getElementById("login_password").focus();
+    return false;
+  } else {
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        login_email: login_email,
+        login_password: login_password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message == "ERROR") {
+          //document.getElementById("message").innerHTML=data.msgtype;
+        }
+
+        if (data.message == "SUCCESS") {
+          location.href = "/thankyou";
+        }
+      });
+  }
 };
