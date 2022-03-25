2e412b7674f92a18b1c40ae01ec614b9b70f6229
umeshchakor30 <umeshchakor30@gmail.com>
2022-03-25 21:11:55 +0530

    validation

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,5 @@
 // For login
+var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 const login = () => {
   var login_email = document.getElementById("login_user").value;
   var login_password = document.getElementById("login_password").value;
@@ -8,7 +9,6 @@ const login = () => {
   document.getElementById("err_login_password").innerHTML = "";
   document.getElementById("err_login_type").innerHTML = "";
   document.getElementById("err_msg").style.display = "none";
-  var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
   if (login_email.length == 0) {
     document.getElementById("err_login_user").innerHTML =
@@ -70,27 +70,69 @@ const signUp = () => {
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
 
-  fetch("/signupProcess", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      first_name: first_name,
-      last_name: last_name,
-      user_email: user_email,
-      username: username,
-      password: password,
-    }),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      if (data.message == "ERROR") {
-        //document.getElementById("message").innerHTML=data.msgtype;
-      }
+  document.getElementById("err_first_name").innerHTML = "";
+  document.getElementById("err_last_name").innerHTML = "";
+  document.getElementById("err_user_email").innerHTML = "";
+  document.getElementById("err_username").innerHTML = "";
+  document.getElementById("err_password").innerHTML = "";
 
-      if (data.message == "SUCCESS") {
-        location.href = "/thankyou";
-      }
-    });
+  if (first_name.length == 0) {
+    document.getElementById("err_first_name").innerHTML =
+      "Please enter a first name";
+    document.getElementById("first_name").focus();
+    return false;
+  } else if (last_name.length == 0) {
+    document.getElementById("err_last_name").innerHTML =
+      "Please enter a last name";
+    document.getElementById("last_name").focus();
+    return false;
+  } else if (user_email.length == 0) {
+    document.getElementById("err_user_email").innerHTML =
+      "Please enter a email id";
+    document.getElementById("user_email").focus();
+    return false;
+  } else if (reg.test(user_email) == false) {
+    document.getElementById("err_user_email").innerHTML =
+      "Please enter a valid email id";
+    return false;
+  } else if (username.length == 0) {
+    document.getElementById("err_username").innerHTML =
+      "Please enter a username";
+    document.getElementById("username").focus();
+    return false;
+  } else if (password.length == 0) {
+    document.getElementById("err_password").innerHTML =
+      "Please enter a password";
+    document.getElementById("password").focus();
+    return false;
+  } else {
+    $(".mj_preloaded").show();
+    $(".mj_preloader").show();
+
+    fetch("/signupProcess", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        first_name: first_name,
+        last_name: last_name,
+        user_email: user_email,
+        username: username,
+        password: password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message == "ERROR") {
+          $(".mj_preloaded").hide();
+          $(".mj_preloader").hide();
+          //document.getElementById("message").innerHTML=data.msgtype;
+        }
+
+        if (data.message == "SUCCESS") {
+          location.href = "/thankyou";
+        }
+      });
+  }
 };
